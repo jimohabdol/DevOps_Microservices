62ee1685481753bc9b3ca97439d5fb11cf3d9e6a
jimohabdol <jimoh_abdol@yahoo.com>
2022-02-26 08:55:48 +0100

    task completed

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+# dockerpath=<> 
+dockerpath=jimohabdol/udacity-prediction-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run prediction-api --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward prediction-api 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=jimohabdol/udacity-prediction-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=jimohabdol
+docker tag udacity-prediction-api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
